140e9e6553678baaef375c0407fa411cd4d0bcad
Ramin Faracov <ramin.faracov@gmail.com>
2021-11-10 23:26:48 +0400

    Add files via upload

---
+++ sample_data.sql
@@ -0,0 +1,208 @@
+CREATE TABLE test (
+	id serial4 NOT NULL,
+	pdata jsonb NULL,
+	CONSTRAINT test_pk PRIMARY KEY (id)
+);
+
+
+insert into test (pdata) values (
+'{
+	"id": 10001,
+	"pin": "1ZJ8MWM",
+	"doc_id": 4397618,
+	"id_text": "10001",
+	"doc_type": 1,
+	"isactive": 1,
+	"insert_date": "2016-07-05T10:08:03.333",
+	"insert_user": {},
+	"mobilenumber": "0507972937"
+}');
+
+insert into test (pdata) values (
+'{
+	"id": 10091,
+	"pin": null,
+	"doc_id": 4589392,
+	"id_text": "10091",
+	"doc_type": 1,
+	"isactive": 1,
+	"insert_date": "2017-08-17T13:06:55.757",
+	"insert_user": {},
+	"mobilenumber": "0518247053"
+}');
+
+insert into test (pdata) values (
+'{
+	"id": 10038,
+	"pin": null,
+	"doc_id": 4593128,
+	"id_text": "10038",
+	"doc_type": 1,
+	"isactive": 1,
+	"insert_date": "2017-08-25T09:10:01.41",
+	"insert_user": {},
+	"mobilenumber": "0558406433"
+}');
+
+insert into test (pdata) values (
+'{
+	"id": 10004,
+	"pin": "216K638",
+	"doc_id": 4338782,
+	"id_text": "10004",
+	"doc_type": 1,
+	"isactive": 1,
+	"insert_date": "2016-02-09T10:51:09.957",
+	"insert_user": {
+		"id": 1,
+		"month": 1,
+		"user_id": 100,
+		"descript": "First Group"
+	},
+	"mobilenumber": "0775133492"
+}');
+
+insert into test (pdata) values (
+'{
+	"id": 10005,
+	"pin": null,
+	"doc_id": 4624667,
+	"id_text": "10005",
+	"doc_type": 1,
+	"isactive": 1,
+	"insert_date": "2017-10-30T09:26:38.61",
+	"insert_user": {
+		"id": 4,
+		"month": 4,
+		"user_id": 100,
+		"descript": "First Group"
+	},
+	"mobilenumber": "0508597319"
+}');
+
+insert into test (pdata) values (
+'{
+	"id": 10020,
+	"pin": "2NJKKWU",
+	"doc_id": 4587588,
+	"id_text": "10020",
+	"doc_type": 1,
+	"isactive": 1,
+	"insert_date": "2017-08-14T15:36:11.827",
+	"insert_user": {
+		"id": 6,
+		"month": 6,
+		"user_id": 100,
+		"descript": "First Group"
+	},
+	"mobilenumber": "0505800100"
+}');
+
+insert into test (pdata) values (
+'{
+	"id": 10021,
+	"pin": null,
+	"doc_id": 4591620,
+	"id_text": "10021",
+	"doc_type": 1,
+	"isactive": 1,
+	"insert_date": "2017-08-22T14:51:11.633",
+	"insert_user": {
+		"id": 8,
+		"month": 8,
+		"user_id": 100,
+		"descript": "First Group"
+	},
+	"mobilenumber": "0508850325"
+}');
+
+
+insert into test (pdata) values (
+'{
+	"id": 10022,
+	"pin": "19YY96P",
+	"doc_id": 4326902,
+	"id_text": "10022",
+	"doc_type": 1,
+	"isactive": 1,
+	"insert_date": "2016-01-05T11:13:59.14",
+	"insert_user": {
+		"id": 12,
+		"month": 12,
+		"user_id": 100,
+		"descript": "First Group"
+	},
+	"mobilenumber": "0515972763"
+}');
+
+
+insert into test (pdata) values (
+'{
+	"id": 10023,
+	"pin": "3E0Q1WW",
+	"doc_id": 4575568,
+	"id_text": "10023",
+	"doc_type": 1,
+	"isactive": 1,
+	"insert_date": "2017-07-20T17:19:03.637",
+	"insert_user": {
+		"id": 15,
+		"month": 3,
+		"user_id": 200,
+		"descript": {}
+	},
+	"mobilenumber": "0508506484"
+}');
+
+
+insert into test (pdata) values (
+'{
+	"id": 10039,
+	"pin": "0X09VA9",
+	"doc_id": 4591952,
+	"id_text": "10039",
+	"doc_type": 1,
+	"isactive": 1,
+	"insert_date": "2017-08-23T10:47:56.017",
+	"insert_user": {
+		"id": 3,
+		"month": 3,
+		"user_id": 100,
+		"descript": {}
+	},
+	"mobilenumber": "0502103411"
+}');
+
+
+insert into test (pdata) values (
+'{
+	"id": 10080,
+	"pin": "6BQKXEF",
+	"doc_id": 4404807,
+	"id_text": "10080",
+	"doc_type": 1,
+	"isactive": 1,
+	"insert_date": "2016-07-20T10:48:23.873",
+	"insert_user": {
+		"id": 17,
+		"month": 5,
+		"user_id": 200,
+		"descript": null
+	},
+	"mobilenumber": "0516992707"
+}');
+
+
+insert into test (pdata) values (
+'{
+	"id": 10091,
+	"pin": null,
+	"doc_id": 4589392,
+	"id_text": "10091",
+	"doc_type": 1,
+	"isactive": 1,
+	"insert_date": "2017-08-17T13:06:55.757",
+	"insert_user": {},
+	"mobilenumber": "0518247053"
+}');
+
